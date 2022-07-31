a6fe932b2a5c5247924152a987ea624ad75e4083
illtron3030 <illtron3030@gmail.com>
2022-07-30 22:31:14 -0700

    some HTML and JS lines

--- script.js
+++ script.js
@@ -8,6 +8,68 @@ var currentQuestionIndex = 0;
 
 var questions = [
     {
-        question: ""
-    }
-]
+        question: "what color is your momma?",
+        options: ["black", "green","purple", "red"],
+        answer: "black"
+    },
+    {
+        question: "what color is your momma?",
+        options: ["black", "green","purple", "red"],
+        answer: "black"
+    },
+    {
+        question: "what color is your momma?",
+        options: ["black", "green","purple", "red"],
+        answer: "black"
+    },
+    {
+        question: "what color is your momma?",
+        options: ["black", "green","purple", "red"],
+        answer: "black"
+    },
+    {
+        question: "what color is your momma?",
+        options: ["black", "green","purple", "red"],
+        answer: "black"
+    },
+    {
+        question: "what color is your momma?",
+        options: ["black", "green","purple", "red"],
+        answer: "black"
+    },
+    {
+        question: "what color is your momma?",
+        options: ["black", "green","purple", "red"],
+        answer: "black"
+    },
+    {
+        question: "what color is your momma?",
+        options: ["black", "green","purple", "red"],
+        answer: "black"
+    },
+    {
+        question: "what color is your momma?",
+        options: ["black", "green","purple", "red"],
+        answer: "black"
+    },
+    {
+        question: "what color is your momma?",
+        options: ["black", "green","purple", "red"],
+        answer: "black"
+    },
+];
+
+function renderCurrentQuestion(){
+    containerEl.innerHTML = '';
+    var currentQuestion = questions [currentQuestionIndex];
+    var header = document.createElement('h2');
+    header.textContent = currentQuestion.question;
+}
+
+startBtn.addEventListener('click', function()) {
+    renderCurrentQuestion();
+var timerInterval = setInterval(function(){
+    timerEl.textContent = timer;
+    timer--;
+}
+}
