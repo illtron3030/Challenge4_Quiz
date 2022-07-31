7e5b98d3eef51ffa0b0f763cbcbc0605ce84def0
illtron3030 <illtron3030@gmail.com>
2022-07-30 21:53:53 -0700

    some JS

---
+++ script.js
@@ -0,0 +1,13 @@
+var contentEl = document.getElementById('content');
+var startBtn = document.querySelector('#start');
+var timerEl = document.querySelector('#timer');
+var containerEl = document.querySelector('#container')
+
+var timer = 5;
+var currentQuestionIndex = 0;
+
+var questions = [
+    {
+        question: ""
+    }
+]
